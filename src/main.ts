beb6f4af2da5ca12b07672f1e47b62c2b34a1fb3
SGVIKA <112934601+SGVIKA@users.noreply.github.com>
2025-07-21 17:16:52 +0300

    add port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api")
+
+  app.enableCors({
+    origin:['http:localhost:3000', 'http://87.228.88.172:3000']
+  })
   await app.listen(process.env.PORT ?? 4200);
 }
 bootstrap();
